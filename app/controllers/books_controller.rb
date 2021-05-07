e0cf9db45afc1671e2d44cd2d105b4d15e901a7d
tkg03boo18 <tkg03boo18@gmail.com>
2021-05-07 07:18:24 +0000

    [Update]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-       flash[:notice] = "Book was successfully created."
+       flash[:notice] = "Book was successfully updated."
        redirect_to book_path(@book.id)
     else
       render 'edit'
@@ -37,8 +37,10 @@ class BooksController < ApplicationController
   end
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
   private
